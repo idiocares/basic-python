75e21f0ff482cd5ee6dad515286d01d8c33fa7ab
Israel Diocares <diocaresisrael@outlook.com.ar>
2021-11-10 18:00:52 -0300

    Bucles repetitivos

---
+++ bucleFor.py
@@ -0,0 +1,27 @@
+
+
+"""for contador in range(1000): #rango  (0,999)
+    print(contador)
+
+
+for i in range(10): #rango (0,9)
+    print (11*i)
+
+"""
+
+"""def run():
+    nombre=input('Escribe tu nombre :')
+    for i in nombre:
+        print(i)
+
+"""
+
+def run():
+    frase = input('escribir una frase: ')
+    for caracter in frase:
+        print(caracter.upper())
+
+if __name__=='__main__':
+    run()
+
+
